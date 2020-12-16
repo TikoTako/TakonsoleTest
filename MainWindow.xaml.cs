55c47d1d6aceedb88a20a5394eb5a93f58b675d0
TikoTako <49921594+TikoTako@users.noreply.github.com>
2020-12-16 04:13:45 +0100

    Update 1.1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+using System.Reflection;
 using System.Windows;
-
-using TikoTako;
+using System.Windows.Controls;
+using takon = TikoTako.Takonsole;
 
 namespace TakonsoleTest
 {
@@ -16,22 +19,25 @@ namespace TakonsoleTest
             InitializeComponent();
         }
 
-        Takonsole con;
-        Action<string> ImGonnaPrintSomething = Takonsole.Out;
-        Action<string> ImGonnaPrintSomethingTwoTheRevenge = Takonsole.Inf;
-        Action<string> W = Takonsole.Warn;
-        Action<string> HoRyShItTo = Takonsole.Err;
+        Action<string> ImGonnaPrintSomething = takon.Out;
+        Action<string> ImGonnaPrintSomethingTwoTheRevenge = takon.Inf;
+        Action<string> W = takon.Warn;
+        Action<string> HoRyShItTo = takon.Err;
+
+        private void ShowErrorDlg(string str) { MessageBox.Show(str, "Error.", MessageBoxButton.OK, MessageBoxImage.Error); }
+        private void ShowInfDlg(string str) { MessageBox.Show(str, "Information.", MessageBoxButton.OK, MessageBoxImage.Information); }
 
         private void Alloc_Click(object sender, RoutedEventArgs e)
         {
-            con = Takonsole.Alloc("Testing console");
-            Takonsole.Out("Standard output with the static method.");
-            Takonsole.Inf("Standard output with the static method.");
-            Takonsole.Warn("Standard output with the static method.");
-            Takonsole.Err("Standard output with the static method.");
-
-            Alloc.IsEnabled = false;
-            Test1.IsEnabled = Test2.IsEnabled = true;
+            if (takon.Alloc("Testing console"))
+            {
+                Alloc.IsEnabled = false;
+                Enumerable.Range(1, 8).ToList().ForEach(i => ((Button)this.FindName($"Test{i}")).IsEnabled = true);
+            }
+            else
+            {
+                ShowErrorDlg("Unable to allocate the console.");
+            }
         }
 
         void UpDown(ref int f, ref int b, int rL)
@@ -47,28 +53,32 @@ namespace TakonsoleTest
             int rL = rbColors.Length - 1,
                 sL = str.Length,
                 f = rL,
-                b = 0;            
+                b = 0;
             for (int i = 0; i < sL; i++)
             {
                 UpDown(ref f, ref b, rL);
-                con.RawPrintToConsole($"#", false, (i == sL - 1), false, rbColors[f], rbColors[b]);
+                takon.Write("#", null, rbColors[f], rbColors[b]);
             }
+            takon.WriteLine();
+
             foreach (var color in rbColors)
             {
                 for (int i = 0; i < sL; i++)
                 {
                     UpDown(ref f, ref b, rL);
-                    con.RawPrintToConsole($"{str[i]}", false, (i == sL - 1), false, rbColors[f], color);
+                    takon.Write("#", null, rbColors[f], color);
                 }
+                takon.WriteLine();
             }
+
             Color c = Color.Black;
-            meIsLoop:
+        meIsLoop:
             foreach (var color in rbColors)
             {
-                con.RawPrintToConsole($"{color.Name}", false, false, false, color, c);
-                con.RawPrintToConsole($"{color.Name}", false, false, false, c, color);
+                takon.Write($"{color.Name}", null, color, c);
+                takon.Write($"{color.Name}", null, c, color);
             }
-            con.RawPrintToConsole(null, false, true, true, null, null);
+            takon.WriteLine();
             if (c.Equals(Color.Black))
             {
                 c = Color.White;
@@ -83,10 +93,161 @@ namespace TakonsoleTest
 
         private void Test2_Click(object sender, RoutedEventArgs e)
         {
-            ImGonnaPrintSomething("Hello.");
-            ImGonnaPrintSomethingTwoTheRevenge("Bye bye.");
-            W("I'm gonna watch netflix soon.");
-            HoRyShItTo("Like in ten minutes, just the time that i check if the code compile...");
+            bool?[] tsF = { null, null, true, false };
+            takon.TimeStampFormat.customFormat = "ddd dd MMMM yyyy HH:mm:ss zzz";
+            foreach (var item in tsF)
+            {
+                takon.TimeStampFormat.dtl = item;
+                ImGonnaPrintSomething("timestamped normal text");
+                takon.Inf("timestamped information text");
+                W("timestamped warning text");
+                takon.Err("timestamped error text");
+                takon.TimeStampFormat.customFormat = ""; // loopderp
+            }
+        }
+
+        private void Test3_Click(object sender, RoutedEventArgs e)
+        {
+            string buff;
+            var tmpColors = Tuple.Create(takon.NormalColor, takon.BackgroundColor, takon.InformationColor, takon.WarningColor, takon.ErrorColor);
+            var arrayOfColors = (typeof(Color).GetProperties(BindingFlags.Static | BindingFlags.DeclaredOnly | BindingFlags.Public));
+
+            Enumerable.Range(0, 255).ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(i, 0, 0), null));
+            Enumerable.Range(0, 255).Reverse().ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(0, i, 0), null));
+            Enumerable.Range(0, 255).ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(0, 0, i), null));
+            Enumerable.Range(0, 255).Reverse().ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(i, i, 0), null));
+            Enumerable.Range(0, 255).ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(i, 0, i), null));
+            Enumerable.Range(0, 255).Reverse().ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(0, i, i), null));
+            Enumerable.Range(0, 255).ToList().ForEach(i => takon.Write($"#", null, Color.FromArgb(i, i, i), null));
+
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"Array of colors (from System.Drawing.Color) = {arrayOfColors.Length}");
+            foreach (var bColor in arrayOfColors)
+            {
+                buff = "";
+                foreach (var fColor in arrayOfColors)
+                {
+                    buff += $"{takon.SetFontColor(Color.FromName(fColor.Name))}{takon.SetBackgroundColor(Color.FromName(bColor.Name))}#{((Console.CursorLeft == Console.WindowWidth) ? Environment.NewLine : "")}";
+                }
+                Console.Write(buff);
+            }
+            (takon.NormalColor, takon.BackgroundColor, takon.InformationColor, takon.WarningColor, takon.ErrorColor) = tmpColors;
+            Console.WriteLine(Environment.NewLine);
+            Console.WriteLine($"done {takon.SetFontColor(Color.BlueViolet)}{takon.SetStyle(takon.Style.Reverse)}DONE{takon.UnSetStyle(takon.Style.Reverse)}{takon.SetFontColor(takon.NormalColor)} done");
+        }
+
+        int currentFont = 0;
+        List<string> fontsInstalled = null;
+
+        private void Test4_Click(object sender, RoutedEventArgs e)
+        {
+            if (fontsInstalled != null && fontsInstalled.Count > 0)
+            {
+                takon.SetFont(fontsInstalled[currentFont], (short)(currentFont + 14));
+                takon.Inf($"The font is now \"{fontsInstalled[currentFont]}\", the size is {(currentFont + 14)}");
+                currentFont += currentFont == fontsInstalled.Count - 1 ? -currentFont : 1;
+            }
+            else if (fontsInstalled == null)
+            {
+                fontsInstalled = new List<string>();
+                string[] fonts = { "Consolas", "Courier New", "DejaVu Sans Mono", "Liberation Mono", "Lucida Console", "Source Code Pro Medium" };
+
+                // Font reqire System.Drawing.Common
+                fontsInstalled.AddRange(from font in fonts
+                                        where new Font(font, 16).Name.Equals(font)
+                                        select font);
+                ShowInfDlg($"Found {fontsInstalled.Count} fonts:{Environment.NewLine}{String.Join(Environment.NewLine, fontsInstalled)}");
+                if(fontsInstalled.Count > 0)
+                {
+                    Test4_Click(null, null);
+                }
+            }
+            else
+            {
+                ShowErrorDlg("Can't find any font.");
+            }
+        }
+
+        object oldColors = Tuple.Create(takon.NormalColor, takon.BackgroundColor, takon.InformationColor, takon.WarningColor, takon.ErrorColor);
+
+        private void Test5_Click(object sender, RoutedEventArgs e)
+        {
+            object tmpColors = oldColors;
+            if (((Button)sender).Name.Equals("Test5"))
+            {
+                // tmpColors = oldColors;
+            }
+            else if (((Button)sender).Name.Equals("Test6"))
+            {
+                tmpColors = Tuple.Create(Color.YellowGreen, Color.PapayaWhip, Color.CornflowerBlue, Color.RosyBrown, Color.OrangeRed);
+            }
+            else if (((Button)sender).Name.Equals("Test7"))
+            {
+                tmpColors = Tuple.Create(Color.Ivory, Color.DarkSlateBlue, Color.NavajoWhite, Color.Coral, Color.Tomato);
+            }
+            (takon.NormalColor, takon.BackgroundColor, takon.InformationColor, takon.WarningColor, takon.ErrorColor) = (Tuple<Color, Color, Color, Color, Color>)tmpColors;
+            takon.CLS();
+            Test2_Click(sender, e);
+        }
+
+        Color InvertColor(Color color)
+        {
+            return color.Name.Contains("ray") ? Color.Black : Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B);
+        }
+
+        private void Test8_Click(object sender, RoutedEventArgs e)
+        {
+            takon.CLS();
+            takon.WriteLine();
+            var arrayOfColors = (typeof(Color).GetProperties(BindingFlags.Static | BindingFlags.DeclaredOnly | BindingFlags.Public));
+            var str = "bebs";
+            int
+                i = 0,
+                ii = 0,
+                pass = 0,
+                totC = arrayOfColors.Length - 1;
+            Color
+                fColor = Color.GreenYellow,
+                bColor = Color.Red;
+
+            for (; i <= totC && pass <= 2;)
+            {
+                if (pass == 0 || pass == 2)
+                {
+                    str = new String(' ', arrayOfColors[i].Name.Length + 2);
+                    bColor = Color.FromName(arrayOfColors[i].Name);
+                }
+                else if (pass == 1)
+                {
+                    bColor = Color.FromName(arrayOfColors[i].Name);
+                    str = $" {arrayOfColors[i].Name} ";
+                    fColor = InvertColor(bColor);
+                }
+
+                if (Console.CursorLeft + str.Length + 2 > Console.WindowWidth)
+                {
+                    if (pass == 2)
+                    {
+                        takon.WriteLine();
+                        ii = i;
+                    }
+                    i = ii;
+                    takon.WriteLine();
+                    pass += pass == 2 ? -2 : 1;
+                }
+                else if (i == totC)
+                {
+                    i = ii;
+                    pass++;
+                    takon.WriteLine();
+                }
+                else
+                {
+                    i++;
+                    takon.Write(" ");
+                    takon.Write(str, null, fColor, bColor);
+                }
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # TakonsoleTest
-<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/Takonsole.png" height="200"/><br/>
+<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/ss%201.png" height="200"/><br/>
+Simple Takonsole test program.
 
-Simple Takonsole test, methods used:
-* Alloc
-* RawPrintToConsole
-* Out, Inf, Warn, Err
-
-I did a simple test with the rainbow colors maybe i add some 24bit-rgb-box-thingie later.
+```
+Nuget:
+> Install-Package Takonsole
+```
 
 **LINKS**
 
 [Takonsole on nuget](https://www.nuget.org/packages/Takonsole/ "Takonsole on nuget")<br/>
 [Takonsole on github](https://github.com/TikoTako/Takonsole "Takonsole on github")
+
+**SCREENSHOTS**
+
+<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/ss%201.png" height="200"/>
+<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/ss%202.png" height="200"/><br/>
+<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/ss%203.png" height="200"/>
+<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/ss%204.png" height="200"/><br/>
+<img alt="screenshot" src="https://raw.githubusercontent.com/TikoTako/TikoTako/main/img/ss%205.png" height="200"/>
